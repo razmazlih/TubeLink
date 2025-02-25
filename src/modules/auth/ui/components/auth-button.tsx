661fcef78beb23bb9e41b3efd0f8e353789dbe3c
Raz <retraz123@gmail.com>
2025-02-25 19:51:27 +0200

    add studio button in user actions

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -2,17 +2,27 @@
 
 import { Button } from '@/components/ui/button';
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
-import { UserCircleIcon } from 'lucide-react';
+import { ClapperboardIcon, UserCircleIcon } from 'lucide-react';
 
 export const AuthButton = () => {
     return (
         <>
             <SignedIn>
-                <UserButton />
+                <UserButton>
+                    <UserButton.MenuItems>
+                        {/* TODO: Add user profile menu button */}
+                        <UserButton.Link
+                            label="Studio"
+                            href="/studio"
+                            labelIcon={<ClapperboardIcon className="size-4" />}
+                        />
+                        <UserButton.Action label="manageAccount" />
+                    </UserButton.MenuItems>
+                </UserButton>
                 {/* Add menu items for studio and User profile */}
             </SignedIn>
             <SignedOut>
-                <SignInButton mode='modal'>
+                <SignInButton mode="modal">
                     <Button
                         variant="outline"
                         className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shadow-none"
