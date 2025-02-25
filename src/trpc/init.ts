4fee183a6ea0f5db348d1a506c108ca2efe4e971
Raz <retraz123@gmail.com>
2025-02-25 13:22:21 +0200

    add ratelimit to trpc requests

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,5 +1,6 @@
 import { db } from '@/db';
 import { users } from '@/db/schema';
+import { ratelimit } from '@/lib/ratelimit';
 import { auth } from '@clerk/nextjs/server';
 import { initTRPC, TRPCError } from '@trpc/server';
 import { eq } from 'drizzle-orm';
@@ -45,6 +46,12 @@ export const protectedProcedure = t.procedure.use(async function isAuthed(
 
     if (!user) {
         throw new TRPCError({ code: 'UNAUTHORIZED' });
+    }
+
+    const { success } = await ratelimit.limit(user.id)
+
+    if (!success) {
+        throw new TRPCError({ code: 'TOO_MANY_REQUESTS' });
     }
 
     return opts.next({
