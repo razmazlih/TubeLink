d2c80655395a16514d3e9ac2ae6b95e1cbfb82b8
Raz <retraz123@gmail.com>
2025-02-25 12:48:06 +0200

    add trpc context + middleware

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -7,6 +7,7 @@ import { createTRPCReact } from '@trpc/react-query';
 import { useState } from 'react';
 import { makeQueryClient } from './query-client';
 import type { AppRouter } from './routers/_app';
+import Superjson from 'superjson';
 export const trpc = createTRPCReact<AppRouter>();
 let clientQueryClientSingleton: QueryClient;
 function getQueryClient() {
@@ -39,7 +40,7 @@ export function TRPCProvider(
     trpc.createClient({
       links: [
         httpBatchLink({
-          // transformer: superjson, <-- if you use a data transformer
+          transformer: Superjson,
           url: getUrl(),
         }),
       ],
